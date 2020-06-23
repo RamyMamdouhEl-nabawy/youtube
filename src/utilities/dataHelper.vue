fcc3eb10c865e1f520c870f3473ed28851b4caf3
Ramy <ramymamdouh.95@gmail.com>
2020-06-23 13:27:34 +0200

    Creating a new utility for dealing with data.

---
+++ src/utilities/dataHelper.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from "axios";
+
+export default {
+  methods: {
+    //get Data from API.
+    async getData(url) {
+      try {
+        const result = await axios.get(url);
+        return result.data;
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    //posting data through API.
+    async postReq(url, reqObj) {
+      try {
+        const result = await axios.post(url, reqObj);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+};
+</script>
