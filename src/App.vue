508394bef82d4ea0fd49827f70bdbdacc5f9bffe
Ramy <ramymamdouh.95@gmail.com>
2020-06-23 13:27:10 +0200

    updating Home /App components, for include new features.

--- src/App.vue
+++ src/App.vue
@@ -4,31 +4,37 @@
       <router-link to="/">Home</router-link>
       <router-view />
     </nav>-->
-    <SearchBar />
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <Home />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+
+    <div class="utube-body">
+      <Home />
+      <!--<Loader loadingText="Loading..." size="md"/>
+      <img alt="Vue logo" src="./assets/logo.png" />-->
+    </div>
   </div>
 </template>
 
 <script>
-import SearchBar from "./components/common/searchBar/SearchBar";
-import HelloWorld from "./components/HelloWorld.vue";
 import Home from "./components/common/home/Home";
 
 export default {
   name: "App",
   components: {
-    SearchBar,
-    HelloWorld,
     Home
+    // HelloWorld,
   }
 };
 </script>
 
 <style lang="scss">
-@import './Colors.scss';
-@import './CommonStyles.scss';
+@import "./Colors.scss";
+@import "./CommonStyles.scss";
+
+.utube-body {
+  margin-top: 4.25rem;
+  @media screen and (min-width: 992px) {
+    margin: 0;
+  }
+}
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -36,7 +42,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  background-color: $page-background;
+  @media screen and (min-width: 992px) {
+    background-color: $page-background;
+  }
   /* margin-top: 60px; */
 }
 </style>
--- src/components/common/home/Home.vue
+++ src/components/common/home/Home.vue
@@ -1,14 +1,77 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }} Ramy Mamdouh</h1>
+  <div>
+    <SearchBar v-on:searchedVideos="updateVideos($event)" />
+    <div class="container">
+      <Loader
+        v-on:loadingDisplay="loaderDisplay($event)"
+        v-if="loaderDisplayToogle"
+        loadingText="Loading..."
+        size="lg"
+      />
+      <div v-for="(searchVideo,index) in searchedVideos" :key="index">
+        <VideoHolder
+          :videoId="searchVideo.id.videoId"
+          :videoDescription="searchVideo.snippet.description"
+          :videoTitle="searchVideo.snippet.title"
+        />
+      </div>
+      <div v-for="(videosList,index) in allVideos" :key="index">
+        <VideoHolder
+          :videoId="videosList.id.videoId"
+          :videoDescription="videosList.snippet.description"
+          :videoTitle="videosList.snippet.title"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import SearchBar from "../searchBar/SearchBar";
+import VideoHolder from "../../reusableComponents/videoHolder/VideoHolder";
+import Loader from "../../reusableComponents/loader/Loader";
+
 export default {
   name: "Home",
   props: {
     msg: String
+  },
+  components: {
+    SearchBar,
+    VideoHolder,
+    Loader
+  },
+  async created() {
+    const key = "AIzaSyCRVjARE3jcSUIFn9j_SWblCsDOOrnFU8w";
+    const baseURL =
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&key=" +
+      key +
+      "&chart=mostPopular&maxResults=5";
+    try {
+      const result = await axios.get(baseURL);
+      if (result.data.items) {
+        this.allVideos = result.data.items;
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  data() {
+    return {
+      searchedVideos: [],
+      allVideos: [],
+      loaderDisplayToogle: false
+    };
+  },
+  methods: {
+    updateVideos: function(updatedVideos) {
+      this.searchedVideos = updatedVideos;
+    },
+    loaderDisplay: function(display) {
+      console.log(display);
+      this.loaderDisplayToogle = display;
+    }
   }
 };
 </script>
