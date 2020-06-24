12d919bf90c11dc42d74574d07c4a968df8bab2d
Ramy <ramymamdouh.95@gmail.com>
2020-06-24 14:15:51 +0200

    adding bootstrapVue into application.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import Router from "./router";
 import "bootstrap";
-import '@fortawesome/fontawesome-free';
+import "bootstrap/dist/css/bootstrap.min.css";
+import BootstrapVue from "bootstrap-vue";
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
