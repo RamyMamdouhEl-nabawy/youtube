3a8f8abed9518195e9954571ea967f17e22bf7a9
ServiceDesk <servicedesk@Rs-MacBook-Pro.local>
2020-08-04 08:54:17 +0200

    Separated component into multiple files.

--- src/components/reusableComponents/loader/Loader.vue
+++ src/components/reusableComponents/loader/Loader.vue
@@ -1,33 +1,6 @@
-<template>
-  <div class="loader-container" v-bind:class=" 'loader-container--' + size">
-    <div class="loader-container__icon fa fa-spinner fa-spin" role="status"></div>
-    <div
-      class="loader-conatiner__text"
-      v-bind:class="loadingText.length > 0 ?  '' : 'loader-container__text--hidden'"
-    >{{loadingText}}</div>
-  </div>
-</template>
+<template src="./Loader.vue.html"></template>
 
-<script>
-export default {
-  name: "Loader",
-  data() {
-    return {
-      withText: false
-    };
-  },
-  props: {
-    loadingText: {
-      type: String,
-      default: ""
-    },
-    size: {
-      type: String,
-      default: "sm"
-    }
-  },
-  computed: {}
-};
+<script src="./Loader.vue.js" lang="js">
 </script>
 
 <style lang="scss" scoped>
